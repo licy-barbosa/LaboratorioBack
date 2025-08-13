a34e4b3c14cce6964b963a6018fb841aedf383c0
licy.barbosa <licy.barbosa@gmail.com>
2025-08-13 14:35:43 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/GenerosController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using LaboratorioBack.Data;
+using LaboratorioBack.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LaboratorioBack.Controllers
+{
+    [Route("api/generos")]
+    [ApiController] //validaciones
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "isadmin")]
+    public class GenerosController : Controller
+    {
+        public IMapper _mapper { get; }
+        private readonly ApplicationDbContex _context;
+
+        public GenerosController(ApplicationDbContex _context, IMapper mapper) { 
+            this._context = _context;
+            this._mapper = mapper;
+        }
+
+        [HttpGet]
+        [OutputCache]
+        public async Task<List<GeneroDTO>> Index()
+        {
+            return await  _context.Generos
+                .ProjectTo<GeneroDTO>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using LaboratorioBack.Model;
+using LaboratorioBack.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace LaboratorioBack.Data
+{
+    //public class ApplicationDbContex : DbContext
+    public class ApplicationDbContex : IdentityDbContext // para utilizar el sistema de ususarios de ASP.NET Identity
+    {
+        protected ApplicationDbContex()
+        {
+        }
+
+        public ApplicationDbContex(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+
+        public DbSet<Estudio> Estudios { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }   
+    }
+}
